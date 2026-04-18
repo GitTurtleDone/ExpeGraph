a21bdc07a8f1a9c9941e96bc5148d3813c17d74e
GitTurtleDone <giang2110@gmail.com>
2026-04-18 18:52:25 +1200

    Implemented Run button, showed graphs in the UI

--- frontend/src/api/acquire.ts
+++ frontend/src/api/acquire.ts
@@ -1,4 +1,3 @@
-
 // const BASE = "http://localhost:5174";
 const WINDOW_BASE = "http://172.31.80.1:8000";
 export async function getAllConnectedEquipment():Promise<string[]>{
@@ -17,11 +16,24 @@ export function runMeasurement(
 ) {
 	// SSE uses EventSource -> can't use fetch for streaming
 	// -> Need to POST first then listen
-	fetch(`${WINDOW_BASE}\measurement\run`, {
+	fetch(`${WINDOW_BASE}/measurement/run`, {
 		method: 'POST',
 		headers: {'Content-Type':'application/json'},
-		body: JSON.stringify({resourceString, swees})
+		body: JSON.stringify({ resource_string: resourceString, sweeps })
 	}).then(async(res) => {
-		const reader 
+		const reader = res.body!.getReader()
+		const decoder = new TextDecoder()
+		while (true) {
+			const { done, value } = await reader.read()
+			if (done) { onDone(); break}
+			const text = decoder.decode(value)
+			// returned SSE lines format: "data: {...}\n\n"
+			text.split("\n").forEach((line) => {
+				if (line.startsWith("data: ")) {
+					const point = JSON.parse(line.slice(6))
+					onPoint(point.voltage, point.current)
+				}
+			})	 
+		}
 	})
 }

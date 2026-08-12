f032732830826e4826ac2d01c510508d8696b61e
GitTurtleDone <giang2110@gmail.com>
2026-08-12 21:56:33 +1200

    Search batches: implementing backend

--- data_management/Migrations/20260812022857_AddBatchFabricationDateIndex.Designer.cs
+++ data_management/Migrations/20260812022857_AddBatchFabricationDateIndex.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using DataManagement.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace DataManagement.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260812022857_AddBatchFabricationDateIndex")]
+    partial class AddBatchFabricationDateIndex
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ data_management/Migrations/20260812022857_AddBatchFabricationDateIndex.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBatchFabricationDateIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_batches_fabrication_date",
+                schema: "expegraph",
+                table: "batches",
+                column: "fabrication_date");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_batches_fabrication_date",
+                schema: "expegraph",
+                table: "batches");
+        }
+    }
+}

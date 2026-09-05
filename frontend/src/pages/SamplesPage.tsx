8df9384a41a3b08291c2822730fe032fa93afa0a
GitTurtleDone <giang2110@gmail.com>
2026-09-06 11:57:22 +1200

    Minor SamplePage.tsx modification

--- frontend/src/pages/SamplesPage.tsx
+++ frontend/src/pages/SamplesPage.tsx
@@ -1,11 +1,23 @@
-import { Typography } from "@mui/material"
-import { useForm} from "react-hook-form";
+import { Typography, Button, IconButton } from "@mui/material";
+import { register } from "plotly.js";
+import { useForm } from "react-hook-form";
+type SampleInputElementLayout = {
+  label: string;
+  optionaltype: string;
+  registered: boolean;
+};
 export default function SamplesPage() {
-  
-  return (
-  <Typography variant="h2" mb={4}> Samples </Typography>
-  // -- Lef panel--
-  // -- Right panel ---
+  const sampleInputElementLayout: SampleInputElementLayout[] = [
+    { label: "ID", type: "number", registered: false },
+    { label: "" },
+  ];
 
-  )
+  return (
+    <Typography variant="h2" mb={4}>
+      {" "}
+      Samples{" "}
+    </Typography>
+    // -- Lef panel--
+    // -- Right panel ---
+  );
 }
